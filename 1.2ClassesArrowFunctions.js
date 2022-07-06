7a8d50359989ca7ce1cb363235ec6f1d29c92afa
rociomarchetti <marchettirociob@gmail.com>
2022-07-06 07:58:50 +0200

    minor changes

--- 1.2ClassesArrowFunctions.js
+++ 1.2ClassesArrowFunctions.js
@@ -1,15 +1,32 @@
+//NIVEL 1
+
 /* - Exercici 1
 Mostra per la consola el resultat d'una arrow function autoinvocable que sumi dos nombres. */
 
-const arrowFunction = (num1, num2) => num1 + num2
-console.log(arrowFunction(3, 7))
+/* const arrowFunction = (num1, num2) => num1 + num2
+console.log(arrowFunction(3, 7)) */
+
+((num1, num2) => console.log(num1 + num2))(3, 7)
+
+//NIVEL 2
+
+/* - Exercici 1
+Crea una arrow function que, rebent un paràmetre, retorni un objecte 
+amb un atribut que tingui com a valor el paràmetre rebut. */
+
+const carColor = (color) => {
+    const car = { car: color }
+    return car
+}
+
+console.log(carColor('rojo'))
 
 /* - Exercici 2
 Crea una classe "Persona" que rebi un paràmetre 'nom' en ser instanciada. 
 La classe inclourà un mètode dirNom que imprimeixi per consola el paràmetre 'nom'. 
 Invoca el mètode dirNom des de fora de la classe. */
 
-class Person {
+/* class Person {
 
     constructor(name) {
         this.name = name
@@ -21,13 +38,29 @@ class Person {
 }
 
 newPerson = new Person()
-newPerson.printName('Pepito')
+newPerson.printName('Pepito') */
+
+class Person {
+
+    constructor(name) {
+        this.name = name
+    }
+
+    printName() {
+        console.log(this.name)
+    }
+}
+
+newPerson = new Person('Pepito')
+newPerson.printName()
+
+//NIVEL 3
 
 /* - Exercici 1
 Escriu una function creadora d'objectes que faci instàncies d'una classe abstracta. 
 Invoca-la amb diferents definicions. */
 
-function createObject() {
+/* function createObject() {
     return new Object
 }
 
@@ -35,4 +68,36 @@ const car = createObject()
 car.color = 'rojo'
 car.wheels = 4
 
-console.log(car)
+console.log(car) */
+
+class Thing {
+
+    constructor() {
+        if (this.constructor === Thing) {
+            throw new Error('That cant be a thing')
+        }
+    }
+
+    sayThings() {
+        throw new Error('Las cosas no hablan!')
+    }
+
+    doThings() {
+        console.log('Its my thing!')
+    }
+
+}
+
+class Moco extends Thing {
+    sayThings() {
+        console.log('Hello Moco')
+    }
+}
+
+
+
+new Moco().doThings()
+new Moco().sayThings()
+
+new Thing()
+new Thing().sayThings()
--- 1.5.js
+++ 1.5.js
@@ -2,11 +2,11 @@
 
 /* - Exercici 1
 Crea una funció que, en executar-la, escrigui una frase en un fitxer. */
- 
+
 let fs = require('fs')
- 
+
 const message = 'Esta es la frase a ser impresa'
- 
+
 fs.writeFile('newfile.txt', message, (err) => {
     if (err) {
         console.log('Error al crear archivo')
@@ -14,17 +14,17 @@ fs.writeFile('newfile.txt', message, (err) => {
         console.log('Archivo creado con éxito')
     }
 })
- 
+
 /* - Exercici 2
 Crea una altra funció que mostri per consola el contingut del fitxer de l'exercici anterior. */
- 
+
 fs.readFile('newfile.txt', 'utf8', (err, data) => {
     if (err) {
         console.log('Error al leer el archivo')
     } else {
         console.log(data)
     }
- 
+
 })
 
 /* - Exercici 3
@@ -33,4 +33,30 @@ Crea una funció que comprimeixi el fitxer del nivell 1. */
 const compression = require('compressing')
 
 compression.gzip.compressFile('newfile.txt', 'compressFile.gz')
-.then(console.log('el archivo ha sido comprimido'))
+    .then(console.log('el archivo ha sido comprimido'))
+
+//NIVEL 2
+
+/* - Exercici 1
+Crea una funció que imprimeixi recursivament un missatge per la consola amb demores d'un segon. */
+
+let limitOfPrints = 5
+let counter = 0
+
+let printRecursiveMessage = function () {
+    setInterval(() => {
+        if (counter < limitOfPrints) {
+            console.log('Esta frase se imprimirá de manera recursiva cada 1 segundo')
+            counter++
+        } else {
+            clearInterval(limitOfPrints)
+        }
+    }, 1000)
+}
+
+printRecursiveMessage()
+
+/* - Exercici 2
+Crea una funció que llisti per la consola el contingut del directori d'usuari/ària de 
+l'ordinador utilizant Node Child Processes. */
+
