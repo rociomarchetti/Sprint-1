fc8bbdf24900c2d86773c85bd8be062b6a18e628
rociomarchetti <marchettirociob@gmail.com>
2022-07-08 12:41:09 +0200

    CRC 1.4

--- 1.4AsyncAwait.js
+++ 1.4AsyncAwait.js
@@ -8,7 +8,7 @@ let employees = [{
     id: 3,
     name: 'Jeff Bezos'
 }];
- 
+
 let salaries = [{
     id: 1,
     salary: 4000
@@ -19,76 +19,58 @@ let salaries = [{
     id: 3,
     salary: 2000
 }];
- 
+
 /* - Exercici 1
 Crea una funció asíncrona que rebi un id d'empleat/da i imprimeixi per pantalla
 el nom de l'empleat/da i el seu salari, usant les funcions getEmployee() i getSalary()
 que has definit a la tasca anterior. */
- 
-//FUNCIONES YA CREADAS
- 
-const getEmployee = valueID => {
- 
-    const employeeInfo = new Promise((resolve, reject) => {
+
+//FUNCIONES YA CREADAS (INICIO) 
+
+const getEmployee = (valueID) => {
+
+    return new Promise((resolve, reject) => {
+
         if (employees.find(employee => employee.id === valueID)) {
             const choosedEmployee = employees.find(employee => employee.id === valueID)
-            const employeeData = `El nombre de ese empleado es: ${choosedEmployee.name}`
-            resolve(employeeData)
+            resolve(choosedEmployee)
         } else {
-            const errorEmployee = 'No ha ingresado un id correcto'
-            reject(console.error(errorEmployee))
-        }  
-    })
- 
-    employeeInfo
-        .then(ok => {
-            console.log(ok)
-        })
-        .catch(errorEmployee => {
-            console.error(errorEmployee)
-        })
- 
- 
-    return employeeInfo
-}
- 
-const getSalary = employeeObject => {
- 
-    const employeeSalary = new Promise((resolve, reject) => {
-        if (employees.includes(employeeObject)) {
-            const thatEmployee = employees.indexOf(employeeObject)
-            const thatEmployeeSalary = `El salario de ese empleado es ${salaries[thatEmployee].salary}`
-            resolve(thatEmployeeSalary)
-        } else {
-            const errorSalary = 'No ha ingresado los datos correctos de un empleado'
-            reject(console.error(errorSalary))
+            reject('error')
         }
     })
- 
-    employeeSalary
-        .then(ok => {
-            console.log(ok)
-        })
-        .catch(errorSalary => {
-            console.error(errorSalary)
-        })
- 
-    return employeeSalary
 }
-//////////////////////////////////////////////////
- 
+
+const getSalary = (valueID) => {
+
+    return new Promise((resolve, reject) => {
+        if (salaries.find(salary => salary.id === valueID)) {
+            const thatEmployee = salaries.find(salary => salary.id === valueID)
+            resolve(thatEmployee)
+        } else {
+            reject('error') 
+        }
+    })
+}
+
+//FUNCIONES YA CREADAS (FIN)
+
 async function getEmployeeData(valueID) {
-    const employeeName = await getEmployee(valueID)
-    const employeeIncome = await getSalary(employees[valueID])
+    const choosedEmployee = await getEmployee(valueID)
+        .then(choosedEmployee => console.log(`Nombre: ${choosedEmployee.name}`))
+        .catch(error => console.log('error'))
+
+    const thatEmployee = await getSalary(valueID)
+        .then(thatEmployee => console.log(`Salario: ${thatEmployee.salary}`))
+        .catch(error => console.log('error'))
+
 }
- 
-let valueID = 2
-getEmployeeData(valueID)
- 
+
+getEmployeeData(1)
+
 /* - Exercici 2
 Crea una nova funció asíncrona que cridi a una altra que retorni
 una Promise que efectuï la seva funció resolve() després de 2 segons de la seva invocació. */
- 
+
 function firstPromise() {
     return new Promise(resolve => {
         setTimeout(() => {
@@ -96,18 +78,18 @@ function firstPromise() {
         }, 2000)
     })
 }
- 
+
 async function testPromise() {
     const result = await firstPromise()
     return result
 }
- 
+
 testPromise()
- 
+
 /* - Exercici 1
 Crea una funció que retorni el doble del número que li passa com a paràmetre després de 2 segons.
 Crea una altra funció que rebi tres números i calculi la suma dels seus dobles fent servir la funció anterior. */
- 
+
 function double(value) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -115,7 +97,7 @@ function double(value) {
         }, 2000)
     })
 }
- 
+
 async function addition(num1, num2, num3) {
     const firstNumber = await double(num1)
     const secondNumber = await double(num2)
@@ -123,21 +105,21 @@ async function addition(num1, num2, num3) {
     const additionResult = firstNumber + secondNumber + thirdNumber
     return additionResult
 }
- 
+
 addition(3, 7, 9)
     .then(result => console.log(result))
- 
+
 /* - Exercici 1
 Força i captura tants errors com puguis dels nivells 1 i 2. */
- 
-getEmployee(4).catch(errorEmployee => console.log(errorEmployee))
-getEmployee(-1).catch(errorEmployee => console.log(errorEmployee))
-getEmployee(null).catch(errorEmployee => console.log(errorEmployee))
-getEmployee(NaN).catch(errorEmployee => console.log(errorEmployee))
-getSalary(4).catch(errorSalary => console.log(errorSalary))
-getSalary(-1).catch(errorSalary => console.log(errorSalary))
-getSalary(null).catch(errorSalary => console.log(errorSalary))
-getSalary(NaN).catch(errorSalary => console.log(errorSalary))
- 
- 
+
+getEmployee(4).catch(error => console.log('El id ingresado no se corresponde con un empleado'))
+getEmployee(-1).catch(error => console.log('El id ingresado no se corresponde con un empleado'))
+getEmployee(null).catch(error => console.log('El id ingresado no se corresponde con un empleado'))
+getEmployee(NaN).catch(error => console.log('El id ingresado no se corresponde con un empleado'))
+getSalary(4).catch(error => console.log('El id ingresado no se corresponde con un salario'))
+getSalary(-1).catch(error => console.log('El id ingresado no se corresponde con un salario'))
+getSalary(null).catch(error => console.log('El id ingresado no se corresponde con un salario'))
+getSalary(NaN).catch(error => console.log('El id ingresado no se corresponde con un salario'))
+
+
 
